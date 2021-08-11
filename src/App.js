dff88ccc09a8893415a48c83ff320d3d711b34b6
Saumy Pandey <shubhamspandey470@gmail.com>
2021-08-12 00:01:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,49 +3,35 @@ import Anmi from './anmi.js';
 import './App.css'
 
 
-
 const Stopwatch = () => {
-const [hour, setHour] = useState(0);
-const [min, setMin] = useState(0);
-const [sec, setSec] = useState(0);
-const [msec, setMsec] = useState(0);
-const [stop, setStop] = useState(true);
-var dic = [];
+    
+    const [hour, setHour] = useState(0);
+    const [min, setMin] = useState(0);
+    const [sec, setSec] = useState(0);
+    const [msec, setMsec] = useState(0);
+    const [stop, setStop] = useState(true);
 
 
 // start 
 const onStart = () => {
     setStop(false);
     setMsec(msec + 1);
-    let temp = `start: ${hour} : ${min} : ${sec} : ${msec}`;
-    alert(temp.type);
-    dic.push(temp);
-   
 }
 
 // stop
 const onStop = () => {
     setStop(true)
-    let temp = `stop: ${hour} : ${min} : ${sec} : ${msec}`;
-    dic.push(temp);
-    
 }
 
 // reset
 const onReset = () => {
-    let temp = `reset: ${hour} : ${min} : ${sec} : ${msec}`;
-    dic.push(temp);
+    
 
     setHour(0);
     setMin(0);
     setSec(0);
     setMsec(0);
-    
 }
-const Rec = () => {
-    console.log(dic);
-}
- 
 
 // to run the timer element
 useEffect(() => {
@@ -91,18 +77,16 @@ useEffect(() => {
                 <div className="watch">
                     <span>{hour} :</span>
                     <span>{min} :</span>
-                    <span>{sec}</span>
+                    <span>{sec}:</span>
                     <span style={{fontSize: 20}}>{msec}</span>
                 </div>
                 
                 
                 <div className="btn">
-                    <button className="btn-start" type="button" name="start" onClick={onStart}>Start</button>
-                    <button className="btn-stop" type="button" name="stop" onClick={onStop}>Stop</button>
-                    <button className="btn-reset" type="button" name="reset" onClick={onReset}>Reset</button>
+                    <button className="btn-start" type="button" onClick={onStart}>Start</button>
+                    <button className="btn-stop" type="button" onClick={onStop}>Stop</button>
+                    <button className="btn-reset" type="button" onClick={onReset}>Reset</button>
                 </div>
-                <br />
-                <button type="button" onClick={Rec}>Daba bsdk</button>
                 <br /> <br />
             </div>
         </div>
